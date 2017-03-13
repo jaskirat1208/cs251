16ff06ed2a606e4a6f5e9cf27507766061cebb83
jaskirat1208 <jaskiratsingh1208@gmail.com>
2017-03-13 13:50:37 +0530

    Assign8

--- Assign8/generation.R
+++ Assign8/generation.R
@@ -1,5 +1,5 @@
 n <- rnorm(100,0.5,0.15)
 u <- runif(100,0.0,1.0)
 
-write(n,file="normal/n10.txt",sep= "\n")
-write(u,file="uniform/u10.txt",sep= "\n")
+# write(n,file="normal/n10.txt",sep= "\n")
+# write(u,file="uniform/u10.txt",sep= "\n")
